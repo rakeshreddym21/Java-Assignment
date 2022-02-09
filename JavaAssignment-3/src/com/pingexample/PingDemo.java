30d13700a5610e640bed4fcfdcbac963288ef9ae
Rakesh Reddy <rakeshreddy.m@zemosolabs.com>
2022-02-09 11:18:57 +0530

    Assignment upload

---
+++ JavaAssignment-3/src/com/pingexample/PingDemo.java
@@ -0,0 +1,10 @@
+package com.pingexample;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.*;
+import java.util.Scanner;
+public class PingDemo {
+
+
+}
---
+++ JavaExamples/src/com/courseexamples/File.java
@@ -0,0 +1,4 @@
+package com.courseexamples;
+
+public class File {
+}
